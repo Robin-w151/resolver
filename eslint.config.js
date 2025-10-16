2a301a44eae1c7e7573f2c888b752c5f2da6873f
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-10-16 12:00:04 +0200

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,9 @@
 import js from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
+import ts from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 
 export default [
   js.configs.recommended,
-  {
-    files: ['src/**/*.ts'],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        ecmaVersion: 'es2023',
-        sourceType: 'module',
-        project: './tsconfig.json',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tseslint,
-    },
-    rules: {
-      ...tseslint.configs.recommended.rules,
-    },
-  },
+  ...ts.configs.recommended,
   prettier,
 ];

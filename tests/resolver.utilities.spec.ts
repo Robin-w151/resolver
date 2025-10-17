e394e73a7bea063156c52f2363955bfbd0d8b847
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-10-17 15:08:29 +0200

    refactor: split tests into separate files

---
+++ tests/resolver.utilities.spec.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test } from 'vitest';
+import type { TaskResult } from '../src/resolver.interface.js';
+import { hasNoErrors, isError, isSuccess } from '../src/resolver.js';
+
+describe('Resolver utilities', () => {
+  test.each([
+    { value: { data: 1 }, expected: true },
+    { value: { error: new Error('Error') }, expected: false },
+  ])('isSuccess $value = $expected', ({ value, expected }) => {
+    expect(isSuccess(value)).toBe(expected);
+  });
+
+  test.each([
+    { value: { data: 1 }, expected: false },
+    { value: { error: new Error('Error') }, expected: true },
+  ])('isError $value = $expected', ({ value, expected }) => {
+    expect(isError(value)).toBe(expected);
+  });
+
+  test.each([
+    { tasks: { A: { data: 1 } }, expected: true },
+    { tasks: { A: { error: new Error('Error') } }, expected: false },
+    { tasks: { A: { data: 1 }, B: { data: 2 } }, expected: true },
+    { tasks: { A: { data: 1 }, B: { error: new Error('Error') } }, expected: false },
+  ])('hasNoErrors $tasks = $expected', ({ tasks, expected }) => {
+    expect(hasNoErrors(tasks as Record<string, TaskResult<unknown>>)).toBe(expected);
+  });
+});
